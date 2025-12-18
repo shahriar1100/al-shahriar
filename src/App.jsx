b5be9b8b955430a05904d3af2d7abeb2c0b8c9cc
alshahriar-dev <sahariarsikder.aa@gmail.com>
2025-12-18 10:27:22 +0600

    education part remove

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
         <Skills />
         <Experience />
         <Work />
-        <Education />
+        {/* <Education /> */}
         <Contact />
         <Footer />
       </div>
